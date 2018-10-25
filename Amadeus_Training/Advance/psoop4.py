852c3442f68cf4d0cb89938b0516448474f318d6
mnanjundegowda <nmanojkumar030@gmail.com>
2018-10-25 06:09:18 +0530

    Adding Python programs

---
+++ Amadeus_Training/Advance/psoop4.py
@@ -0,0 +1,24 @@
+"""
+    Package Manager
+
+    name
+    version
+
+"""
+
+
+class PackageManager:
+    def __init__(self, name, version):
+        self.__name = name  # __name - private
+        self.version = version
+
+    def __get_information(self):
+        print('name :', self.__name)
+        print('version :', self.version)
+
+    def wrapper(self):
+        self.__get_information()
+
+
+pm = PackageManager('pip', '2.2.18')
+pm.wrapper()
