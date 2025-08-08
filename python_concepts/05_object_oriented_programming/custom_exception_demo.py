e9c82da850cfa41c8d6e8dab86a07999dfa3aa0f
Manoj Kumar N <nmanojkumar030@gmail.com>
2025-08-08 15:25:54 +0530

    Refactor

--- python_concepts/05_object_oriented_programming/custom_exception_demo.py
+++ python_concepts/05_object_oriented_programming/custom_exception_demo.py
@@ -1,7 +1,5 @@
 """
-
 Implementing Custom Exception
-
 """
 
 
@@ -29,11 +27,13 @@ class Connections:
             raise TooManyConnectionError("Too many concurrent connections")
 
     def __str__(self):
-        return "[{} connection id = {}]".format(self.__class__.__name__, self.connection_id)
+        return "[{} connection id = {}]".format(
+            self.__class__.__name__, self.connection_id
+        )
 
 
 # Main method
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         list_of_connection = list()
         for item in range(1, 7):
